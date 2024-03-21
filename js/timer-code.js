b78de51af2a51b154bbd38e1243b9611a0758281
SergeyChumak <schumak@sportmaster.ru>
2024-03-21 23:35:53 +0300

    blinking

--- js/timer-code.js
+++ js/timer-code.js
@@ -66,7 +66,7 @@ function enable_disable_duel_options_conrols(visibility, disabled) {
     document.getElementById("start_stop_timer").style.visibility = visibility;
     document.getElementById("change_player").style.visibility = visibility;
     document.getElementById("protest").style.visibility = visibility;
-    document.getElementById("pause").style.visibility = visibility;   
+    document.getElementById("pause").style.visibility = visibility;
     document.getElementById("Player1Name").disabled = disabled;
     document.getElementById("Player2Name").disabled = disabled;
     document.getElementById("Choose_File_Button").disabled = disabled;
@@ -76,8 +76,8 @@ function enable_disable_duel_options_conrols(visibility, disabled) {
     document.getElementById("4min").disabled = disabled;
     document.getElementById("1min").disabled = disabled;
     document.getElementById("dice_button").disabled = disabled;
-    
-    
+
+
 }
 
 function start_duel() {
@@ -174,8 +174,7 @@ function triggerClick() {
     JSON_File.click()
 }
 
-function processDuelsJson(file)
-{
+function processDuelsJson(file) {
     const fileName = document.getElementById('file-name');
     const duelChooser = document.getElementById('duel-chooser');
 
@@ -204,38 +203,38 @@ function loadFile(event) {
     //var file = event.target.files[0];
     if (file) {
         var reader = new FileReader();
-        duelsList={};
+        duelsList = {};
         var fileExtension = file.name.split('.').pop();
         if (fileExtension === 'xlsx') {
             reader.onload = function handleFileLoad() {
-                                    var arrayBuffer = this.result,
-                                        array = new Uint8Array(arrayBuffer),
-                                        binaryString = String.fromCharCode.apply(null, array);
-                                    /* Call XLSX */
-                                    var workbook = XLSX.read(binaryString, { type: "binary" });
-                                    /* DO SOMETHING WITH workbook HERE */
-                                    var first_sheet_name = workbook.SheetNames[0];
-                                    /* Get worksheet */
-                                    var worksheet = workbook.Sheets[first_sheet_name];
-                                    duelsList = XLSX.utils.sheet_to_json(worksheet, { raw: true });
-                                    for (var i in duelsList) {
-                                                var duel = duelsList[i];
-                                                duel.SituationRoles = JSON.parse(duel.SituationRoles.trim().replace(/^"(.*)"$/, '$1'));
-                                        };
-                                    processDuelsJson(file);
-                                };
-            reader.readAsArrayBuffer(file);  
-        }                        
+                var arrayBuffer = this.result,
+                    array = new Uint8Array(arrayBuffer),
+                    binaryString = String.fromCharCode.apply(null, array);
+                /* Call XLSX */
+                var workbook = XLSX.read(binaryString, { type: "binary" });
+                /* DO SOMETHING WITH workbook HERE */
+                var first_sheet_name = workbook.SheetNames[0];
+                /* Get worksheet */
+                var worksheet = workbook.Sheets[first_sheet_name];
+                duelsList = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+                for (var i in duelsList) {
+                    var duel = duelsList[i];
+                    duel.SituationRoles = JSON.parse(duel.SituationRoles.trim().replace(/^"(.*)"$/, '$1'));
+                };
+                processDuelsJson(file);
+            };
+            reader.readAsArrayBuffer(file);
+        }
         else if (fileExtension === 'json') {
             reader.onload = function handleFileLoad(evt) {
-                                 duelsList = JSON.parse(evt.target.result);
-                                processDuelsJson(file);
-                             }
+                duelsList = JSON.parse(evt.target.result);
+                processDuelsJson(file);
+            }
             reader.readAsText(file, "UTF-8");
-        } 
+        }
         else {
             alert('Unsupported file format. Please select a .xlsx or .json file.');
-        }  
+        }
 
         reader.onerror = function (evt) { console.error(evt); }
     }
@@ -246,15 +245,15 @@ function duelChoosed(currentDuelRef) {
     if (currentDuel != "-1") {
         const duel = duelsList[currentDuel]
         document.getElementById("players-name").innerHTML = `Ситуация №${duel.SituationNum} ${duel.SituationName}`;
-        document.getElementById("Player1Name").value =duel.Player1;
+        document.getElementById("Player1Name").value = duel.Player1;
         document.getElementById("Player2Name").value = duel.Player2;
         document.getElementById("Duel_Num").textContent = "Ситуация №" + duel.SituationNum + ". \"" + duel.SituationName + "\"";
-        document.getElementById("Duel_Text").innerHTML = duel.SituationDescription;        
+        document.getElementById("Duel_Text").innerHTML = duel.SituationDescription;
         var select1 = document.getElementById('Player1Roles');
         var select2 = document.getElementById('Player2Roles');
         clearSelectOptions('Player1Roles');
         clearSelectOptions('Player2Roles');
-        var RolesText="<b>Роли и интересы:</b>";
+        var RolesText = "<b>Роли и интересы:</b>";
         for (var i in duel.SituationRoles) {
             var sitRoles = duel.SituationRoles[i];
             var opt1 = document.createElement('option');
@@ -265,8 +264,8 @@ function duelChoosed(currentDuelRef) {
             opt2.innerHTML = sitRoles.Role;
             select1.appendChild(opt1);
             select2.appendChild(opt2);
-            RolesText+= "<br><b>"+ duel.SituationRoles[i].Role+ "</b> - "+ duel.SituationRoles[i].Goals;
-        };     
+            RolesText += "<br><b>" + duel.SituationRoles[i].Role + "</b> - " + duel.SituationRoles[i].Goals;
+        };
         document.getElementById("Duel_Roles").innerHTML = RolesText;
 
     }
@@ -295,34 +294,46 @@ function setDuelTime(time) {
     initTimers();
 }
 
-function ShowHideSituationInfo()
-{
-    
-    if (!document.getElementById("hide_text").checked)
-    {      
+function ShowHideSituationInfo() {
+
+    if (!document.getElementById("hide_text").checked) {
         document.getElementById("PlayersInterests").style.visibility = 'hidden';
         document.getElementById("SituationText").style.visibility = 'hidden';
     }
-    else
-    {
+    else {
         document.getElementById("PlayersInterests").style.visibility = 'visible';
         document.getElementById("SituationText").style.visibility = 'visible';
     }
-    
+
 }
 /*---------------------dice ---------------------------------*/
 
 
 function dice() {
-    const dice0 = document.getElementById("dice0")
-    dice0.style.transform = 'rotate(1080deg)'
+
     setTimeout(() => {
-        dice0.style.transform = 'rotate(0deg)'
         if (Math.random() >= 0.5) {
             document.getElementById("Player1Name").value = duelsList[currentDuel].Player2;
-            document.getElementById("Player2Name").value = duelsList[currentDuel].Player1;    
+            document.getElementById("Player2Name").value = duelsList[currentDuel].Player1;
         }
         document.getElementById("Player1Label").style.backgroundColor = inactivePlayerColor;
-        document.getElementById("Player2Label").style.backgroundColor = inactivePlayerColor;    
-    }, 1000)
+        document.getElementById("Player2Label").style.backgroundColor = inactivePlayerColor;
+    }, 1100)
+
+    blinking(1000 + Math.ceil(Math.random() * 1000), 75)
+}
+
+function blinking(count, step) {
+    const a = document.getElementById("Player1Name").value
+    const b = document.getElementById("Player2Name").value
+
+    document.getElementById("Player1Name").value = b
+    document.getElementById("Player2Name").value = a
+
+    setTimeout(() => {
+        count = count - step
+        if (count > 0) {
+            blinking(count, step)
+        }
+    }, step)
 }
